6cc9202a18dad1a23fd30f67cf738a024886a02c
Hustcketlyj <1347481211@qq.com>
2024-04-12 19:02:56 +0800

    Update util.py

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import base64
-
+from PIL import Image
 def load_image_local(file_path):
-    with open(file_path, "rb") as img_file:
-        base64_data = base64.b64encode(img_file.read()).decode('utf-8')
-        return f"data:image/png;base64,{base64_data}"
+    image = Image.open(file_path)
+    return image
+    #with open(file_path, "rb") as img_file:
+    #    base64_data = base64.b64encode(img_file.read()).decode('utf-8')
+    #    uri=f"data:image/png;base64,{base64_data}"
